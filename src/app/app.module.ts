8aec6e30f19345e5f86573028a83f64af1fa19bf
labcode-de <leo@labcode.de>
2017-11-25 11:56:48 +0100

    init ng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from "@angular/router";
+import { StudentInterfaceComponent } from './student-interface/student-interface.component';
+const appRoutes: Routes = [
+  { path: 'student',
+    component: StudentInterfaceComponent
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentInterfaceComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
