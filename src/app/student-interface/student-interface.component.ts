44d0fd203f0a13fd2b2366e9d250655b704cc0fb
Rebecca <pfauenauge@posteo.de>
2017-11-25 13:42:56 +0100

    pckg.json modified

--- src/app/student-interface/student-interface.component.ts
+++ src/app/student-interface/student-interface.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare let $: any
 @Component({
   selector: 'app-student-interface',
   templateUrl: './student-interface.component.html',
@@ -10,6 +10,10 @@ export class StudentInterfaceComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+	  $(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
   }
 
 }
